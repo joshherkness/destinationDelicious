d359484c250c1de581f05f16d5c9ff1ee6616161
Joseph Herkness <jpherkness@gmail.com>
2017-04-07 22:54:48 -0400

    Added a report service to handle creating and getting reports

---
+++ services/ReportService.js
@@ -0,0 +1,77 @@
+import * as firebase from 'firebase';
+
+const geofire = require('geofire');
+const geofireRef = new geofire(firebase.database().ref("geofire"))
+
+class ReportService {
+
+  /**
+   * Adds a route object to the firebase database.
+   * @param {Dict} report
+   */
+  static createReport(report) {
+    var user = firebase.auth().currentUser
+    var newReportKey = firebase.database().ref().child('reports').push().key;
+    firebase.database().ref('reports/' + newReportKey).set({
+      user: user.uid,
+      name: report.name,
+      description: report.description,
+      foodtype: report.foodtype,
+      longitude: report.longitude,
+      latitude: report.latitude
+    });
+
+    // Add the report key and location to geofire so we can do lookups quickly
+    geofireRef.set({ [newReportKey] : [report.latitude, report.longitude] }).then(function() {
+      console.log("Report has been added to GeoFire");
+    }, function(error) {
+      console.log("Error: " + error);
+    });
+  }
+
+  /**
+   * Asynchronous updates the caller on reports within a radius of the provided location
+   * @param {Dict} center
+   * @param {Number} radius
+   * @param {Function} added
+   * @param {Function} moved
+   * @param {Function} removed
+   */
+  static getReports(center, radius, entered, exited, moved) {
+    var geoQuery = geofireRef.query({
+      center: [center.latitude, center.longitude],
+      radius: radius
+    });
+
+    var onKeyEnteredRegistration = geoQuery.on("key_entered", function(key, location, distance) {
+      ReportService.getReport(key, function(report){
+        entered(report)
+      });
+    });
+
+    var onKeyEnteredRegistration = geoQuery.on("key_exited", function(key, location, distance) {
+      ReportService.getReport(key, function(report){
+        exited(report)
+      });
+    });
+
+    var onKeyEnteredRegistration = geoQuery.on("key_moved", function(key, location, distance) {
+      ReportService.getReport(key, function(report){
+        moved(report)
+      });
+    });
+  }
+
+  /**
+   * Asynchronous returns a route object given an id
+   * @param {String} id
+   * @param {Function} callback
+   */
+  static getReport(id, callback) {
+    firebase.database().ref('/reports/' + id).once('value').then(function(snapshot) {
+      callback(snapshot.val());
+    });
+  }
+}
+
+module.exports = ReportService
