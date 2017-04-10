94ebd04e66ef5e5bf465a6371b66c0c7fedc6b55
Ian Teal <iateal@oakland.edu>
2017-04-10 16:00:32 -0400

    Code fix

--- components/AccountView.js
+++ components/AccountView.js
@@ -1,23 +1,16 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
-<<<<<<< HEAD
 import { HomeTabs } from '../config/router';
 import * as firebase from 'firebase';
-=======
-<<<<<<< HEAD
+
 import { HomeTabs } from '../config/router';
 import * as firebase from 'firebase';
-=======
->>>>>>> origin/DD-14
->>>>>>> a5251decc2e4ccb4ac15b13cc0a731ca9e3aafa7
+
 
 class AccountView extends Component{
 
 static navigationOptions = {
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> a5251decc2e4ccb4ac15b13cc0a731ca9e3aafa7
+
       title: 'AccountView',
       header: ({ navigate, state }) => ({
         tintColor: '#fff',
@@ -33,17 +26,13 @@ static navigationOptions = {
         )
       }),
     };
-<<<<<<< HEAD
-=======
-=======
+
     tabBar: {
       label: 'Account Settings',
       left: (
                 <Button color='#55acee' title='Sign Out' onPress={() => state.params.signOut() }/>
               )
     }
->>>>>>> origin/DD-14
->>>>>>> a5251decc2e4ccb4ac15b13cc0a731ca9e3aafa7
 
      constructor(props) {
         super(props);
@@ -52,34 +41,22 @@ static navigationOptions = {
        render() {
           return (
             <View style={ styles.container }>
-<<<<<<< HEAD
+
               <Text style={ styles.bigFont }>Account View Screen</Text>
-=======
-<<<<<<< HEAD
+
               <Text style={ styles.bigFont }>Account View Screen</Text>
-=======
+
               <Text style={ styles.bigFont }>AccountView Screen</Text>
->>>>>>> origin/DD-14
->>>>>>> a5251decc2e4ccb4ac15b13cc0a731ca9e3aafa7
+
             </View>
           )
         }
   }
 }
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> a5251decc2e4ccb4ac15b13cc0a731ca9e3aafa7
 
 componentDidMount() {
     this.props.navigation.setParams({ signOut: this.signOut });
 
-
-<<<<<<< HEAD
-=======
-=======
->>>>>>> origin/DD-14
->>>>>>> a5251decc2e4ccb4ac15b13cc0a731ca9e3aafa7
 const styles = StyleSheet.create({
           container: {
              flexDirection: 'column',
